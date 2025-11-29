edae01e9fbf0026ca6f7283a2da170a3080ab8e2
ziad abdul samad <dev.ziad.abdul.samad@gmail.com>
2025-11-29 20:36:26 +0300

    font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import Providers from "./providers";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 export const metadata = {
   title: "Simplics",
   description: "Company portfolio",
@@ -12,10 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={poppins.variable}>
       <body className="min-h-screen bg-background text-foreground">
         <Providers>
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
